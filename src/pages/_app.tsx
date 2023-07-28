1578d99b36647ecc81072ae1bfd0b47f23c89b59
Daniel <zrelick@gmail.com>
2023-07-28 22:20:13 +0200

    analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>);
 };
 
 export default MyApp;
